21772af47cae1aee7261db27a83ec698ebdfa96e
Reika Akuzawa <68085523+Reikasan@users.noreply.github.com>
2021-12-01 00:19:20 +0100

    Create Config.php

---
+++ classes/Config.php
@@ -0,0 +1,8 @@
+<?php
+    class Config {
+        const SMTP_HOST = 'HOST-NAME';
+        const SMTP_PORT = PORT-No;
+        const SMTP_USER = 'USERNAME';
+        const SMTP_PASSWORD = 'PASSWORD';
+    }
+?>
