a1cf768d5118928b083f151ef35438bcba89cb41
Reika Akuzawa <l06981@googlemail.com>
2022-01-28 12:37:03 +0100

    fixed favicon bug

--- includes/header.php
+++ includes/header.php
@@ -27,10 +27,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
         
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="img/logo/favicon_reika/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="img/logo/favicon_reika/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="img/logo/favicon_reika/favicon-16x16.png">
-    <link rel="manifest" href="img/logo/favicon_reika/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="favicon.ico">
         
     <!-- Font Awsome -->
     <script src="https://kit.fontawesome.com/fbe614a2b2.js" crossorigin="anonymous"></script>
