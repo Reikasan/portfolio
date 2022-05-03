9c43d6759aa239e884736728d9d19bc777da5a11
Reika Akuzawa <l06981@googlemail.com>
2022-05-03 11:27:32 +0200

    [bug]blanc slideshow #2

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ var scrollDownAndShow = function() {
         icon.classList.add("icon-hide");
     }
 };
-
 window.addEventListener("scroll", scrollDownAndShow);
+
 
 /* SLIDESHOW IN PC */
 const screenshots = document.querySelectorAll('.slide');
@@ -30,135 +30,140 @@ const slideContainer = document.getElementById('slide-container');
 var i = 1;
 var slideShowTimer = setInterval(slideShow, 4000);
 
-    // Add Eventlistener to all tabs 
-    tabs.forEach(tab => {
-        tab.addEventListener('click', ()=> {
-            stopSlideShow();
+// Auto slideshow
+function slideShow(){
+    i++;
+    
+    var selectedTab = defineSelectedTab();
+    var selectedScreenshot = defineSelectedScreenshot();
 
-            var selectedTab = defineSelectedTab();
-            var selectedScreenshot = defineSelectedScreenshot();
-            removeSelectedClass(selectedTab, selectedScreenshot)
-            tab.classList.add('selected');
-             
-            var selectedIndex = defineIndexOfSelectedTab();
+    removeSelectedClass(selectedTab, selectedScreenshot);
 
-            if(selectedIndex === 0) {
-                screenshots[0].classList.add('selected');
-                i = 1;
-            } else if(selectedIndex === 1) {
-                screenshots[1].classList.add('selected');
-                i = 2;
-            } else if(selectedIndex === 2) {
-                screenshots[2].classList.add('selected');
-                i = 3;
-            } else if(selectedIndex === 3) {
-                screenshots[3].classList.add('selected');
-                i = 4;
-            }
-        });
-    });
-
-    // HELPER FUNCTIONS
-    function defineSelectedTab() {
-        return document.querySelector('.tab.selected');
-    }
-    function defineSelectedScreenshot() {
-        return document.querySelector('.slide.selected');
-    }
-
-    function removeSelectedClass(selectedTab, selectedScreenshot) {
-        selectedTab.classList.remove('selected'); 
-        selectedScreenshot.classList.remove('selected');
-    }
-
-    function defineIndexOfSelectedTab() {
-        const newSelectedTab = defineSelectedTab();
-
-        tabs = Array.from(tabs);
-        return tabs.indexOf(newSelectedTab);
-    }
-
-    function stopSlideShow() {
-        clearInterval(slideShowTimer);
-        slideContainer.classList.remove('slider-on');
-        changeOperatorIcon();
-    }
-
-    // Add Eventlistener to all Pause/Play Icons
-    pauseIcons.forEach(pauseIcon => {
-        pauseIcon.removeAttribute('arial-hidden');
-        pauseIcon.addEventListener('click', stopSlideShow);
-    });
-
-    playIcons.forEach(playIcon => {
-        playIcon.removeAttribute('arial-hidden');
-        playIcon.addEventListener('click', function() {
-            // i = defineIndexOfSelectedTab()+1;
-            slideShowTimer = setInterval(slideShow, 4000);
-            slideContainer.classList.add('slider-on');
-            changeOperatorIcon();
-        });
-    });
-
-
-    // Auto slideshow
-    function slideShow(){
-        i++;
+    if(i > screenshots.length) {
+        backToFirstScreenshot()
         
+        i = 1;
+    } else {
+        moveToNextScreenshot()
+    }
+}
+
+// Add Eventlistener to all tabs 
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=> {
+        stopSlideShow();
+
         var selectedTab = defineSelectedTab();
         var selectedScreenshot = defineSelectedScreenshot();
+        removeSelectedClass(selectedTab, selectedScreenshot)
+        tab.classList.add('selected');
+            
+        var selectedIndex = defineIndexOfSelectedTab();
 
-        removeSelectedClass(selectedTab, selectedScreenshot);
-
-        if(i > screenshots.length) {
+        if(selectedIndex === 0) {
             screenshots[0].classList.add('selected');
-            tabs[0].classList.add('selected');
-            
             i = 1;
-        } else {
-            selectedScreenshot.nextElementSibling.classList.add('selected');
-            selectedTab.nextElementSibling.classList.add('selected');
+        } else if(selectedIndex === 1) {
+            screenshots[1].classList.add('selected');
+            i = 2;
+        } else if(selectedIndex === 2) {
+            screenshots[2].classList.add('selected');
+            i = 3;
+        } else if(selectedIndex === 3) {
+            screenshots[3].classList.add('selected');
+            i = 4;
         }
+    });
+});
+
+// Add Eventlistener to all Pause/Play Icons
+pauseIcons.forEach(pauseIcon => {
+    pauseIcon.removeAttribute('arial-hidden');
+    pauseIcon.addEventListener('click', stopSlideShow);
+});
+
+playIcons.forEach(playIcon => {
+    playIcon.removeAttribute('arial-hidden');
+    playIcon.addEventListener('click', function() {
+        slideShowTimer = setInterval(slideShow, 4000);
+        slideContainer.classList.add('slider-on');
+        changeOperatorIcon();
+    });
+});
+
+// Show Pause or Play Icon by hover screen
+slideContainer.addEventListener('mouseover', function() {
+    mouseOverScreens.forEach(screen => {
+        screen.classList.add('show');
+    });
+    changeOperatorIcon();
+});
+
+slideContainer.addEventListener('mouseleave', function() {
+    mouseOverScreens.forEach(screen => {
+        screen.classList.remove('show');
+    });
+    changeOperatorIcon();
+});
+
+
+function changeOperatorIcon() {
+    if(slideContainer.classList.contains('slider-on')) {
+        // show PAUSE icon
+        pauseIcons.forEach(pauseIcon => {
+            pauseIcon.classList.remove('hide');
+        });
+        playIcons.forEach(playIcon => {
+            playIcon.classList.add('hide');
+        });
+        
+    } else {
+        // show PLAY icon
+        pauseIcons.forEach(pauseIcon => {
+            pauseIcon.classList.add('hide');
+        });
+        playIcons.forEach(playIcon => {
+            playIcon.classList.remove('hide');
+        });
     }
+}
 
-    // Show Pause or Play Icon by hover screen
-    slideContainer.addEventListener('mouseover', function() {
+// HELPER FUNCTIONS
+function defineSelectedTab() {
+    return document.querySelector('.tab.selected');
+}
+function defineSelectedScreenshot() {
+    return document.querySelector('.slide.selected');
+}
 
-        mouseOverScreens.forEach(screen => {
-            screen.classList.add('show');
-        });
-        changeOperatorIcon();
-    });
+function removeSelectedClass(selectedTab, selectedScreenshot) {
+    selectedTab.classList.remove('selected'); 
+    selectedScreenshot.classList.remove('selected');
+}
 
-    slideContainer.addEventListener('mouseleave', function() {
+function defineIndexOfSelectedTab() {
+    const newSelectedTab = defineSelectedTab();
 
-        mouseOverScreens.forEach(screen => {
-            screen.classList.remove('show');
-        });
-        changeOperatorIcon();
-    });
+    tabs = Array.from(tabs);
+    return tabs.indexOf(newSelectedTab);
+}
+
+function backToFirstScreenshot() {
+    screenshots[0].classList.add('selected');
+    tabs[0].classList.add('selected');
+}
+
+function moveToNextScreenshot() {
+    selectedScreenshot.nextElementSibling.classList.add('selected'); //
+    selectedTab.nextElementSibling.classList.add('selected');
+}
+
+function stopSlideShow() {
+    clearInterval(slideShowTimer);
+    slideContainer.classList.remove('slider-on');
+    changeOperatorIcon();
+}
     
-
-    function changeOperatorIcon() {
-        if(slideContainer.classList.contains('slider-on')) {
-            // show PAUSE icon
-            pauseIcons.forEach(pauseIcon => {
-                pauseIcon.classList.remove('hide');
-            });
-            playIcons.forEach(playIcon => {
-                playIcon.classList.add('hide');
-            });
-            
-        } else {
-            // show PLAY icon
-            pauseIcons.forEach(pauseIcon => {
-                pauseIcon.classList.add('hide');
-            });
-            playIcons.forEach(playIcon => {
-                playIcon.classList.remove('hide');
-            });
-        }
-    }
 
 /* CONFIRMATION MODAL */
 const submitBtn = document.getElementById('submitBtn');
